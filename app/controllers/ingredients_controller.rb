0b873185c8019774ad8aaaa3b4659a53be2b7dcd
Kwibe Merci <jeremiahkwibe@hotmail.com>
2020-07-24 11:57:30 -0600

    Fixes

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -1,16 +1,6 @@
 class IngredientsController < ApplicationController
     def show
-        chef = Chef.find(params[:id])
-        dishes = chef.dishes.pluck(:id)
-        ingredient_ids = dishes.flat_map do |id|
-            DishIngredient.where(dish_id: id).pluck(:ingredient_id)
-        end
-
-        @ingredients = ingredient_ids.map do |id|
-            Ingredient.find(id)
-        end
-        # Can do better than this but was caught up with time
-        # logic has to be in models
+        @chef = Chef.find(params[:id])
     end
     
 end
--- app/models/chef.rb
+++ app/models/chef.rb
@@ -2,4 +2,8 @@ class Chef <ApplicationRecord
   validates_presence_of :name
   has_many :dishes
 
+  def ingredients
+    self.dishes.select('ingredients.name').joins(:ingredients).distinct
+  end
+  
 end
